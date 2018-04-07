fa766d583668eee0aef404279ef98cb5474b5c34
Dustin Anderson <dandersonsoftware@outlook.com>
2018-04-07 04:16:27 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,18 +1,17 @@
 require 'pry'
 
 class Hash
-  class Hash
-    def keys_of(*args)
-        matches = []
-        self.map do |key, values|
-            args.each do |arg|
-                if values.include?(arg.to_s)
-                    matches << key
-                end
-            end
-        end
-        matches
-    end
 
-end
+  def keys_of(*args)
+      matches = []
+      self.map do |key, values|
+          args.each do |arg|
+              if values.include?(arg.to_s)
+                  matches << key
+              end
+          end
+      end
+      matches
+  end
+
 end
