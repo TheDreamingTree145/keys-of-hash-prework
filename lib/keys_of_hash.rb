86aad73919fe25ea007d9a925b8a1e03d68d62a3
Dustin Anderson <dandersonsoftware@outlook.com>
2018-04-07 04:20:22 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -6,7 +6,6 @@ class Hash
       matches = []
       self.map do |key, value|
           args.each do |arg|
-              binding.pry
               if values.include?(arg)
                   matches << key
               end
